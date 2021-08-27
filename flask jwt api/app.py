07b215b1256a70c4482530c97203eb65ed134e86
jagtap <suraj100jagtap@gmail.com>
2021-08-27 14:40:37 +0530

    token success

--- flask jwt api/app.py
+++ flask jwt api/app.py
@@ -1,27 +1,45 @@
 from flask import Flask, jsonify, request, make_response
+import jwt 
+import datetime
+from functools import wraps
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'thisisthesecretkey'
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token') 
+        if not token:
+            return jsonify({'message' : 'Token is missing!'}), 403
+        try: 
+            data = jwt.decode(token, app.config['SECRET_KEY'],algorithm="HS256")
+            print(data)
+        except jwt.exceptions.InvalidSignatureError:
+            return jsonify({'message' : 'Token is invalid!'}), 403
+        return f(*args, **kwargs)
+    return decorated
 
-
-@app.route('/unprotected',methods=[])
+@app.route('/unprotected')
 def unprotected():
-    return ''
+    return jsonify({'message' : 'Anyone can view this!'})
 
-@app.route('/protected',methods=[])
+@app.route('/protected')
+@token_required
 def protected():
-    return ''
+    return jsonify({'message' : 'This is only available for people with valid tokens.'})
 
 @app.route('/login')
 def login():
     auth = request.authorization
 
-    if auth and auth.password == 'password':
-        return ''
-    return make_response('Could Verify!',401,{'WWW-Authenticate':'Basic realm="Login Required"'})
+    if auth and auth.password == 'suraj':
+        token = jwt.encode({'user' : auth.username}, app.config['SECRET_KEY'],algorithm="HS256")
 
+        return jsonify({'token' : token.decode('UTF-8')})
 
+    return make_response('Could not verify!', 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'})
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
